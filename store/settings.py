ea6ab464578acfdd168b865c4d41f0cfd500f89f
argamManukyan <manoukian00@gmail.com>
2021-08-08 09:06:09 +0400

    change the paths to python-api/

--- store/settings.py
+++ store/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'python-api/static/'
+MEDIA_URL = 'python-api/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static_dev']
