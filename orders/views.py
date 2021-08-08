6a2a4ace1091b0b534db04e289f7c4d2e1c7075b
argamManukyan <manoukian00@gmail.com>
2021-08-09 01:14:30 +0400

    showing token

--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 def generate_token():
     b64Val = base64.b64encode(bytes('api@си:wZi1QsfD'.encode('utf-8')))
     token = requests.post('https://online.moysklad.ru/api/remap/1.2/security/token', headers={"Authorization": "Basic %s" % b64Val}).json()
-    
+    print(token)
     return token.get('access_token')
 
 class FetchBarcodes(APIView):
